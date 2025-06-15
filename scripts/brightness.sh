c7f469e5d67e0d69584aebd7810766b8b07f1f4f
benconte201 <benconte201@gmail.com>
2025-06-15 23:49:08 +0200

    added brightness controll with notification

---
+++ scripts/brightness.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+case "$1" in
+    up)
+        brightnessctl -q s +10%
+        ;;
+    down)
+        brightnessctl -q s 10%-
+        ;;
+    *)
+        echo "Usage: $0 {up|down}"
+        exit 1
+        ;;
+esac
+
+# Get current brightness percentage
+current=$(brightnessctl g)
+max=$(brightnessctl m)
+percentage=$((current * 100 / max))
+
+# Send notification with progress bar
+notify-send "Brightness" "${percentage}%" \
+    -h int:value:${percentage} \
+    -h string:synchronous:brightness \
+    -t 1500 \
+    -i display-brightness-symbolic
